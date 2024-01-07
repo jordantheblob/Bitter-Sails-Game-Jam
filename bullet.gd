45b394ad9c285eb7ec1b83cd43ef91b731c3388b
Noohand <noah.Greasley@outlook.com>
2024-01-07 17:00:54 +0000

    bullet now moves towards mouse position

--- bullet.gd
+++ bullet.gd
@@ -1,11 +1,14 @@
 extends Node2D
 var _speed = 10
 var _angle 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_angle = PI
-
+	var mouse_position = get_global_mouse_position()
+	var direction = mouse_position - position
+	_angle = direction.angle()
 
 func _process(delta):
 	position.x += cos(_angle)*_speed
 	position.y += sin(_angle)*_speed
+	
